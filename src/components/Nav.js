62b4fcaf1e2552b1a92f22e9be13197a6cded552
tarunmahi <tarunmahendra007@gmail.com>
2023-04-21 16:01:36 +0530

    working on navigation bar

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "../styles/Nav.css"
+
+export default function Nav(){
+    return(
+        <div className="tittle">
+        <h2>My Inspirations and idols </h2>
+        </div>
+    )
+
+}
